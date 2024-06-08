118e68d6319e9089e5a4ceddf8cb3179c044b08b
김보민 <bomin@Bomin-MacBook.local>
2024-06-09 02:04:00 +0900

    Style: column컴포넌트 반응형 구현

--- src/app/dashboard/[dashboardid]/page.tsx
+++ src/app/dashboard/[dashboardid]/page.tsx
@@ -22,6 +22,13 @@ const columnMockData = {
       createdAt: '2024-05-31T16:47:59.846Z',
       updatedAt: '2024-05-31T16:47:59.846Z',
     },
+    {
+      id: 2,
+      title: 'On Progress',
+      teamId: 'string',
+      createdAt: '2024-05-31T16:47:59.846Z',
+      updatedAt: '2024-05-31T16:47:59.846Z',
+    },
   ],
 };
 
@@ -33,23 +40,19 @@ export default function dashboardPage() {
   };
 
   return (
-    <>
-      <div className='flex flex-wrap bg-custom_gray-_fafafa'>
-        {/* 컬럼 컴포넌트 뿌리기 */}
-        {columnMockData.data.map((column: any, index: number) => {
-          return <Column key={column.id} title={column.title} />;
-        })}
-        {/* 카드 추가하기 모달 */}
-        <button
-          className='border-gray-_d9d9d9 relative left-[20px] top-[68px] flex h-[70px] w-[354px] items-center justify-center rounded-lg border bg-white'
-          onClick={() => handleOpenModal(<NewColumnModal />)}
-        >
-          <p className='mr-[12px] text-[16px] font-bold'>
-            새로운 컬럼 추가하기
-          </p>
-          <ChipAddIcon size={'large'} />
-        </button>
-      </div>
-    </>
+    <div className='relative flex overflow-x-auto bg-custom_gray-_fafafa max-lg:flex-col'>
+      {/* 컬럼 컴포넌트 뿌리기 */}
+      {columnMockData.data.map((column: any, index: number) => {
+        return <Column key={column.id} title={column.title} />;
+      })}
+      {/* 카드 추가하기 모달 */}
+      <button
+        className='border-gray-_d9d9d9 left-[20px] top-[68px] flex h-[70px] min-w-[354px] items-center justify-center rounded-lg border bg-white max-lg:w-full'
+        onClick={() => <NewColumnModal />}
+      >
+        <p className='mr-[12px] text-[16px] font-bold'>새로운 컬럼 추가하기</p>
+        <ChipAddIcon size={'large'} />
+      </button>
+    </div>
   );
 }
